7994dad553400a1234349eb52921d8746573db8e
tiago_lyrio <tiago.lyrio@ammovarejo.com.br>
2024-09-13 13:28:19 -0300

    fix: correção do envio para multiplos destinatários

--- src/innovation_messenger/__init__.py
+++ src/innovation_messenger/__init__.py
@@ -4,56 +4,57 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from typing import Callable, Dict, Optional
+from typing import Callable, Dict, List, Optional, Union
 
-from commom.base_classes.base_sender import BaseSender
+from commom.base_classes.base_sender import BaseMessenger
+from innovation_messenger.config import config
 
 
 @dataclass
-class IEmailSender:
-    file_name: Optional[str]
+class IEmailProperties:
     subject: str
     body: str
-    recipient: str
+    recipient: Union[str, List[str]]
+    file_name: Optional[str] = None
 
 
-class Messenger(BaseSender):
+class Messenger(BaseMessenger):
     def __init__(self) -> None:
         pass
 
     @classmethod
-    def send_report(
+    def send_message(
         cls,
-        report_channel: str,
+        channel: str,
         **kwargs,
     ) -> None:
-        report_channels: Dict[str, Callable] = {"email": cls._send_email}
+        channels: Dict[str, Callable] = {"email": cls._send_email}
 
-        report_channels[report_channel](**kwargs)
+        channels[channel](**kwargs)
 
     @classmethod
-    def _send_email(cls, email_sender: IEmailSender, **kwargs):
+    def _send_email(cls, email_properties: IEmailProperties, **kwargs):
         message = MIMEMultipart()
-        sender_email = "inovacao@ammovarejo.com.br"
-        message["Subject"] = email_sender.subject
+        sender_email = config.EMAIL_ACCOUNT
+        message["Subject"] = email_properties.subject
         message["From"] = sender_email
-        # message["To"] = email_sender.recipient
-        message["To"] = ",".join(email_sender.recipient)
-        html_part = MIMEText(email_sender.body)
+        if isinstance(email_properties.recipient, List):
+            message["To"] = ", ".join(email_properties.recipient)
+        html_part = MIMEText(email_properties.body)
         message.attach(html_part)
 
-        if email_sender.file_name:
-            with open(email_sender.file_name, "rb") as attachment:
+        if email_properties.file_name:
+            with open(email_properties.file_name, "rb") as attachment:
                 # Add the attachment to the message
                 part = MIMEBase("application", "octet-stream")
                 part.set_payload(attachment.read())
             encoders.encode_base64(part)
             part.add_header(
                 "Content-Disposition",
-                f"attachment; filename= {email_sender.file_name}",
+                f"attachment; filename= {email_properties.file_name}",
             )
             message.attach(part)
 
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-            server.login(sender_email, "@ed8Q124")
-            server.sendmail(sender_email, email_sender.recipient, message.as_string())
+            server.login(sender_email, config.EMAIL_PASSWORD)
+            server.sendmail(sender_email, email_properties.recipient, message.as_string())
