cfdd651aad2122b1caa5182df94223ee985a9df3
joao-garcia-ammovarejo <joao.garcia@ammovarejo.com.br>
2024-09-16 13:49:49 -0300

    create github actions - 3

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,9 @@ echo "export image name"
 export DOCKER_IMAGE_NAME="$GCP_ARTIFACT_REGISTRY_REGION-docker.pkg.dev/$GCP_PROJECT_ID/$GCP_APP_NAME-$ENV/$GCP_IMAGE_NAME:$VERSION"
 
 echo "build image $DOCKER_IMAGE_NAME"
-docker buildx build --platform linux/amd64 --push -t $DOCKER_IMAGE_NAME ./
+# docker buildx build --platform linux/amd64 --push -t $DOCKER_IMAGE_NAME ./
 # docker buildx build --platform linux/amd64 --push -t $DOCKER_IMAGE_NAME --build-arg SSH_PRIVATE_KEY=$SSH_PRIVATE_KEY ./
+docker buildx build --platform linux/amd64 --push -t $DOCKER_IMAGE_NAME --build-arg GCP_CREDENTIALS_SECRET=$GCP_CREDENTIALS_SECRET ./
 
 echo "finish export image"
 gcloud run services update $GCP_APP_NAME-$ENV --image $DOCKER_IMAGE_NAME --platform managed --region $GCLOUD_RUN_REGION --port 8080
