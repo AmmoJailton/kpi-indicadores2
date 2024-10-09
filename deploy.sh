2d9bc84557fc692f8fa3736357b04414a97ab517
joao-garcia-ammovarejo <joao.garcia@ammovarejo.com.br>
2024-10-09 11:44:06 -0300

    change version to timestamp in docker image

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,15 @@ echo "clar docker cache"
 docker buildx prune --all
 
 echo "Get version"
-export VERSION="v"`grep "__version__" src/innovation_api/__init__.py | sed 's/[ "]//g'| sed 's/\./-/g'|cut -d"="  -f2`
+# export VERSION="v"`grep "__version__" src/innovation_api/__init__.py | sed 's/[ "]//g'| sed 's/\./-/g'|cut -d"="  -f2`
+export VERSION=$(date +"%Y%m%d.%H.%M")
 echo $VERSION
 
 echo "export image name"
 export DOCKER_IMAGE_NAME="$GCP_ARTIFACT_REGISTRY_REGION-docker.pkg.dev/$GCP_PROJECT_ID/$GCP_APP_NAME-$ENV/$GCP_IMAGE_NAME:$VERSION"
 
 echo "build image $DOCKER_IMAGE_NAME"
-docker buildx build --platform linux/amd64 --push -t $DOCKER_IMAGE_NAME ./ --build-arg GCP_CREDENTIALS_SECRET_ENCODED=$GCP_CREDENTIALS_SECRET_ENCODED --build-arg EMAIL_SENDER_ACCOUNT=$EMAIL_SENDER_ACCOUNT --build-arg EMAIL_PASSWORDS=$EMAIL_PASSWORDS --build-arg INSTAGRAM_SCRAPPER_API_HOST=$INSTAGRAM_SCRAPPER_API_HOST --build-arg INSTAGRAM_SCRAPPER_API_TOKEN=$INSTAGRAM_SCRAPPER_API_TOKEN
+docker buildx build --platform linux/amd64 --push -t $DOCKER_IMAGE_NAME ./ --build-arg GCP_CREDENTIALS_SECRET_ENCODED=$GCP_CREDENTIALS_SECRET_ENCODED --build-arg EMAIL_SENDER_ACCOUNT=$EMAIL_SENDER_ACCOUNT --build-arg EMAIL_PASSWORDS=$EMAIL_PASSWORDS
 
 echo "finish export image"
 gcloud run services update $GCP_APP_NAME-$ENV --image $DOCKER_IMAGE_NAME --platform managed --region $GCLOUD_RUN_REGION --port 8080
