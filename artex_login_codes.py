d59fa2819b5d9cbf3d4e50ae2120c4870601e6f5
rochiesa <rodrigo.chiesa@coteminas.com.br>
2024-11-01 11:11:06 -0300

    Update artex_login_codes.py

--- artex_login_codes.py
+++ artex_login_codes.py
@@ -1,12 +1,5 @@
 from typing import List
 
 ARTEX_LOGIN_CODES: List[str] = [
-    "XPTO",
-    "1a0b2c3d",
-    "QUalquerValor",
-    "Valor-123",
-    "codigo-123",
-    "123-codigo",
-    "1234hgjgier",
-    "loko",
+    "4mm0",
 ]
