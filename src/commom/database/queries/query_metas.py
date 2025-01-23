5ff4e114a5f6365df578b324891c072684cbc7e0
joao-garcia-ammovarejo <joao.garcia@ammovarejo.com.br>
2025-01-23 09:12:30 -0300

    criacao do fluxo de homologacao

--- src/commom/database/queries/query_metas.py
+++ src/commom/database/queries/query_metas.py
@@ -12,6 +12,5 @@ SELECT
 FROM `projetoomni.isa_workspace.fat_metas`
 
 WHERE EXTRACT(YEAR FROM dia) >= 2024 AND
-tipo_meta IN ('Meta Loja PDV', 'Meta Vendedor PDV') AND
-dia <= CURRENT_DATE()  - 1
+tipo_meta IN ('Meta Loja PDV', 'Meta Vendedor PDV')
 """
